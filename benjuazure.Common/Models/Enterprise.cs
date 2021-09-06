d384d23ea2095d71020a4c4e93483e618c8713e6
YEISON DAVID BENJUMEA GUTIERREZ <yeisonbenjumea187359@correo.itm.edu.co>
2021-09-05 19:47:01 -0500

    Add  Entity

---
+++ benjuazure.Common/Models/Enterprise.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace benjuazure.Common.Models
+{
+    public class Enterprise
+    {
+        public int EmployeId { get; set; }
+        public DateTime Date { get; set; }
+        public int Type { get; set; }
+        public bool IsConsolidated { get; set; }
+    }
+}
---
+++ benjuazure.Functions/Entities/TimeEntity.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace benjuazure.Functions.Entities
+{
+    public class TimeEntity : TableEntity
+    {
+        public int EmployeId { get; set; }
+        public DateTime Date { get; set; }
+        public int Type { get; set;}
+        public bool IsConsolidated { get; set; }
+
+    }
+}
